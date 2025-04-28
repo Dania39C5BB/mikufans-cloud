f651e9177bf61bf169f53d371e472e68e81698de
李自超 <2079253084@qq.com>
2025-04-28 19:39:40 +0800

    init: 搭建微服务基础架构
    - 创建 BaseMapper 通用 Mapper 接口
    - 定义 BusinessException 业务异常类
    - 实现 BaseParam/SimplePage 分页基础参数
    - 封装 ResponseVO 统一响应模型

---
+++ mikufans-cloud-common/src/main/java/com/linyajin/mikufans/entity/query/BaseParam.java
@@ -0,0 +1,12 @@
+package com.linyajin.mikufans.entity.query;
+
+
+import lombok.Data;
+
+@Data
+public class BaseParam {
+	private SimplePage simplePage;
+	private Integer pageNo;
+	private Integer pageSize;
+	private String orderBy;
+}
